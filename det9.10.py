6df3ab65a7d793e466f139a25fb5dd29daf9830a
Eljesa Kqiku <eljesakqiku@gmail.com>
2025-05-16 09:43:16 +0200

    Added dataset after removing outliers

---
+++ det9.10.py
@@ -0,0 +1,9 @@
+# import tensorflow as tn
+
+import pandas as pd 
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, SimpleRNN
+from sklearn.preprocessing import MinMaxScaler
+from sklear.metrics import mean_squared_error
+import math
